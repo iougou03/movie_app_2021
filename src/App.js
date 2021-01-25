5d55244f7b36d879e8a023fc8ca88fcdcefa01fc
iougou03 <haesol820@naver.com>
2021-01-25 16:30:56 +0900

    start project!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+function Food({name}){
+  console.log(name);
+  return <h1>I Like {name}</h1>;
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello!</h1>
+      <Food 
+        name="Haesol"
+      />
+      <Food 
+        name="Kim"
+      />
     </div>
   );
 }
