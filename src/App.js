d09bfd71ec93f1762aa0208db52c8a4d57921cdb
iougou03 <haesol820@naver.com>
2021-02-15 22:08:15 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css"
 
 class App extends React.Component{
   state = {
@@ -14,6 +15,8 @@ class App extends React.Component{
         } 
       } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
       this.setState({ movies , isLoading : false}); 
+
+      // console.log(movies);
   }
 
   componentDidMount(){
@@ -23,16 +26,46 @@ class App extends React.Component{
   render(){
     const {isLoading,movies} = this.state;
     return( 
-      <div>
-        {isLoading ? "Loading...":
-          movies.map(element => {
-            console.log(element.id)
-            return < Movie key={element.id} id={element.id} title={element.title} poster = {element.medium_cover_image} year={element.year} summary={element.summary}/>;
-          })
-        }
-      </div>
-      );  
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ):( 
+          <div className="box">
+            <div className="headerBox">
+              <img src="https://www.gstatic.com/youtube/img/tvfilm/movies_profile.png" alt=""/>
+              <span className="headerText"> 영화 </span>
+            </div>
+            <div className="movies">
+              { movies.map(function(movie){
+                  let temp = String(movie.rating);
+                  if(temp.length<3){
+                    temp+=".0";
+                  }
+                  movie.rating =temp;
 
+                  return(
+                    <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                    rating={movie.rating}
+                    runtime={movie.runtime}
+                  />
+                  );
+                })
+              }
+            </div>
+          </div>
+          
+        )}
+      </section>
+      );  
   }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({id,title,summary,poster,year}){
+function Movie({title,summary,poster,year,genres, rating, runtime}) {
     return(
-        <div>
-            <h1>{title} - {year}</h1>
-            <img src={poster} alt="" />
+        <div className="movie">
+            <div className = "movieThumbnail">
+                <img src={poster} alt={title} title={title}/>
+                <div className="movieRating">
+                    {rating}
+                </div>
+                <div className = "textBlock">
+                    <p>{runtime}min </p>
+                </div>
+            </div>
+            <div className="movie_data">
+                <h3 className="movie_title" >{title}</h3>
+                <h5 className="movie_year">{year}</h5>
+                <ul className="genres">{genres.map((genre, index)=><li key={index} className="genres_genre">{genre}</li>)}</ul>
+                <p className="movie_summary">{}</p>
+            </div>
         </div>
     );
 }
@@ -15,7 +29,10 @@ Movie.propTypes = {
     title : PropTypes.string.isRequired,
     summary : PropTypes.string.isRequired,
     poster : PropTypes.string.isRequired,
-    year : PropTypes.number.isRequired
+    year : PropTypes.number.isRequired,
+    genres : PropTypes.arrayOf(PropTypes.string).isRequired,
+    runtime : PropTypes.number.isRequired,
+    rating : PropTypes.string.isRequired
 };
 
 export default Movie;
